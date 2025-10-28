920826d58ce3c5d429a8e3a9d0d35e7af12f833f
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-10-28 10:57:57 +0100

    Run script regenerates gradle-wrapper.jar

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,8 @@
 @echo off
 
+@REM regenerate gradle-wrapper.jar (it's not pushed to Git)
+call gradle wrapper
+
 @REM call gradlew.bat :app:run --no-configuration-cache
 @REM configuration cache already disabled in gradle.properties
 call gradlew.bat :app:run
